0d32f2a0ae43c445e90f9d935054bd39578d574a
Boubakar PI <163854543+BoubakarPI@users.noreply.github.com>
2024-10-15 17:29:36 +0000

    Starting

---
+++ inertia/pages/app.tsx
@@ -0,0 +1,10 @@
+export default function App () {
+    return (
+        <>
+        <p className="bg-black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque at suscipit eius laboriosam maiores sapiente magni reiciendis exercitationem rem commodi. Aliquid unde fugiat ab quo alias mollitia, distinctio aut dignissimos!
+        </p>
+        </>
+    )
+
+}
